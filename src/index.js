5d35947849cb03e2f39a07a86ef32204b10aeb7d
Marcus Little <marclouisiana16@gmail.com>
2019-05-21 09:40:08 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Background from "./components/Background/Background";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
